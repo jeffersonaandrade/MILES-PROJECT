937ef7662ffb17d17d558d62945dd3d58f169258
Jefferson Andrade <jefferson.a.andrade@hotmail.com>
2024-06-05 16:23:45 -0300

    melhoria no header quando se trata de navegação responsiva.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,24 +8,36 @@ function Header() {
         <header className={styles.header}>
              <div><strong>Vupt!</strong></div>
         <div>
-            <ol className={styles.listaHeader}>
-                <li>Home</li>
-                <li>Produto</li>
-                <li>Sobre</li>
+            <ol className={styles.listaHeaderDesktop}>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Produto</a></li>
+                <li><a href='#'>Sobre</a></li>
             </ol>
         </div>
+        <div className={styles.mobileMenu}>
+            <button className={styles.hamburguerButton}><i className="fas fa-bars"></i></button>
+            {/*<button className={styles.buttonCloseHeader}><i className="fas fa-times"></i></button>*/}
         <div>
             <ol className={styles.listaHeaderMobile}>
-                <li>Home</li>
-                <li>Produto</li>
-                <li>Sobre</li>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Produto</a></li>
+                <li><a href='#'>Sobre</a></li>
+                <li><a href='#'>Buscar</a></li>
+                <li><a href='#'>Anunciar</a></li>
             </ol>
         </div>
+        </div>
         <div>
-            <ol className={styles.listaHeader}>
-                <li>Buscar</li>
-                <li>Anunciar</li>
-                <li>Configs</li>
+            <ol className={styles.listaHeaderDesktop}>
+                <div id='searchMiles' className={styles.searchMiles}>
+                    <i className="fa fa-search" aria-hidden="true"></i>
+                    <li><a href='#'>Buscar</a></li>
+                </div>
+                <div id='addMiles' className={styles.addMiles}>
+                    <i className="fa fa-plus" aria-hidden="true"></i>
+                    <li><a href='#'>Anunciar</a></li>
+                </div>
+                <li><button className={styles.hamburguerButton}><i className="fas fa-bars"></i></button></li>
             </ol>
         </div>
         </header>
